03e0d46585cb7be81583ae89879f5def6f2c53a3
DoradR <dominik.radziszewski99@gmail.com>
2022-04-13 21:04:49 +0200

    Add first model

--- Oderka/admin.py
+++ Oderka/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Match, ResultOfMatch
 
-# Register your models here.
+@admin.register(Match)
+class MatchAdmin(admin.ModelAdmin):
+    list_display = ['opponent', 'odraGoal', 'opponentGoal']
+    list_filter = ('opponent',)
+    search_fields = ('opponent', 'date')
+
+admin.site.register(ResultOfMatch)
--- Oderka/models.py
+++ Oderka/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class ResultOfMatch(models.Model):
+    RESULT = {
+        (0, 'Win'),
+        (1, 'Tie'),
+        (2, 'Lose')
+    }
+
+    matchResult = models.PositiveSmallIntegerField(default=0, choices=RESULT)
+
+class Match(models.Model):
+    opponent = models.CharField(max_length=64, null=False, blank=False)
+    odraGoal = models.PositiveSmallIntegerField(default=0, null=False, blank=False)
+    opponentGoal = models.PositiveSmallIntegerField(default=0, null=False, blank=False)
+    date = models.DateField(null=False, blank=False)
+    result = models.OneToOneField(ResultOfMatch, on_delete=models.CASCADE, null=False, blank=False)
+
+
+    def __str__(self):
+        return self.match_result()
+
+    def match_result(self):
+        return "Odra Słup {} : {} {}".format(self.odraGoal, self.opponent, self.opponentGoal)
