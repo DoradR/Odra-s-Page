26760d5258eb588f991b6d3fc0aeeca1a09dfcbe
DoradR <dominik.radziszewski99@gmail.com>
2022-04-14 18:18:37 +0200

    Add models (Club, Player, ResultOfMatch)

--- Oderka/admin.py
+++ Oderka/admin.py
@@ -1,10 +1,25 @@
 from django.contrib import admin
-from .models import Match, ResultOfMatch
+from .models import Match, ResultOfMatch, Player, Club
+
 
 @admin.register(Match)
 class MatchAdmin(admin.ModelAdmin):
     list_display = ['opponent', 'odraGoal', 'opponentGoal']
     list_filter = ('opponent',)
     search_fields = ('opponent', 'date')
+
+
+@admin.register(Player)
+class PlayerAdmin(admin.ModelAdmin):
+    list_display = ['firstName', 'secondName']
+    list_filter = ('club',)
+    search_fields = ('firstName', 'secondName')
+
+
+@admin.register(Club)
+class PlayerAdmin(admin.ModelAdmin):
+    list_display = ['nameOfClub']
+    search_fields = ('nameOfClub',)
+
 
 admin.site.register(ResultOfMatch)
--- Oderka/models.py
+++ Oderka/models.py
@@ -1,24 +1,40 @@
 from django.db import models
 
+
+class Club(models.Model):
+    nameOfClub = models.CharField(max_length=32, null=False, blank=False)
+    description = models.TextField(max_length=255, null=True, blank=True)
+
+    def __str__(self):
+        return self.nameOfClub
+
+
 class ResultOfMatch(models.Model):
-    RESULT = {
-        (0, 'Win'),
-        (1, 'Tie'),
-        (2, 'Lose')
-    }
-
-    matchResult = models.PositiveSmallIntegerField(default=0, choices=RESULT)
-
-class Match(models.Model):
-    opponent = models.CharField(max_length=64, null=False, blank=False)
-    odraGoal = models.PositiveSmallIntegerField(default=0, null=False, blank=False)
-    opponentGoal = models.PositiveSmallIntegerField(default=0, null=False, blank=False)
-    date = models.DateField(null=False, blank=False)
-    result = models.OneToOneField(ResultOfMatch, on_delete=models.CASCADE, null=False, blank=False)
-
+    matchResult = models.CharField(max_length=10, null=False, blank=False)
 
     def __str__(self):
         return self.match_result()
 
     def match_result(self):
-        return "Odra Słup {} : {} {}".format(self.odraGoal, self.opponent, self.opponentGoal)
+        return "{}".format(self.matchResult)
+
+
+class Match(models.Model):
+    opponent = models.ForeignKey(Club, on_delete=models.CASCADE)
+    odraGoal = models.PositiveSmallIntegerField(default=0, null=False, blank=False)
+    opponentGoal = models.PositiveSmallIntegerField(default=0, null=False, blank=False)
+    date = models.DateField(null=False, blank=False)
+    result = models.ForeignKey(ResultOfMatch, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.match_result()
+
+    def match_result(self):
+        return "Odra Słup {} : {} {}".format(self.odraGoal, self.opponentGoal, self.opponent)
+
+
+class Player(models.Model):
+    firstName = models.CharField(max_length=32, null=False, blank=False)
+    secondName = models.CharField(max_length=32, null=False, blank=False)
+    birth = models.DateField(null=True, blank=False)
+    club = models.ForeignKey(Club, on_delete=models.CASCADE)
