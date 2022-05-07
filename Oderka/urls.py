9776ea184ae5d7eeb1fa9dac238c4f6f64b7a8dd
DoradR <dominik.radziszewski99@gmail.com>
2022-05-07 14:03:58 +0200

    Create Details Page

--- Oderka/urls.py
+++ Oderka/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import main_page, matches_page
+from .views import main_page, matches_page, details_page
 
 urlpatterns = [
     path('front/', main_page, name="main_page"),
-    path('matches/', matches_page, name="matches_page")
+    path('matches/', matches_page, name="matches_page"),
+    path('details/<int:id>/', details_page, name="details_page")
 ]
--- Oderka/views.py
+++ Oderka/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from .models import Match
+from django.shortcuts import render, get_object_or_404
+from .models import Match, Details
 
 
 def main_page(request):
@@ -9,3 +9,13 @@ def main_page(request):
 def matches_page(request):
     matches = Match.objects.all()
     return render(request, 'mecze.html', {'mecze': matches})
+
+
+def details_page(request, id):
+    match = get_object_or_404(Match, pk=id)
+    details = Details.objects.filter(match=match)
+
+    if request.method == "POST":
+        match.objects.filter(id)
+
+    return render(request, 'detale.html', {'detale': details})
